037af4d92144714c1e9f1eba7f58c4119d5f538e
Vasugan11 <triapolitanets@gmail.com>
2024-11-14 19:41:03 +0400

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,44 @@
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self,name):
+        self.name = name
+
+    def eat(self, food):
+        self.food = food
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Plant:
+    edible = False
+    def __init__(self,name):
+        self.name = name
+
+class Mammal(Animal):
+     pass
+class Predator(Animal):
+     pass
+class Flower(Plant):
+     pass
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p4 = Flower('Майский ландыш')
+p2 = Fruit('Заводной апельсин')
+p3 = Fruit('Мочёный ананас')
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p3)
+a2.eat(p4)
+print(a1.alive)
+print(a2.fed)
